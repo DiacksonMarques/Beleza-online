66f983302da84ffc1a302585f1a6e2edffa329ab
Diackson Marques <!D2D3d4d5d>
2019-11-04 00:56:47 -0300

    Mudanças e bugs sendo corrigido IMG

--- app/src/main/java/com/evo/belezaonline_2/Editperfactivity.java
+++ app/src/main/java/com/evo/belezaonline_2/Editperfactivity.java
@@ -57,105 +57,19 @@ public class Editperfactivity extends AppCompatActivity {
 
         btImgPerf = findViewById(R.id.btImgPerf);
 
-        Intent intent = this.getIntent();
+        final Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         idg = bundle.getString("id");
         nome = bundle.getString("nome");
 
-        requestMutiPermission();
-
         btImgPerf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, GALERIA);
+                Intent intent1 = new Intent(getBaseContext(),ImgCActivity.class);
+                intent1.putExtra("id",idg);
+                intent1.putExtra("nome",nome);
+                startActivity(intent1);
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_CANCELED){
-            return;
-        }
-        if(requestCode == GALERIA){
-            if(data == null){
-                assert data != null;
-                Uri contenURI = data.getData();
-                try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contenURI);
-                    btImgPerf.setImageBitmap(bitmap);
-                    uploadImage(bitmap);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getBaseContext(), "Falha", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getBaseContext(), "Falha", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    private void uploadImage(@NotNull Bitmap bitmap){
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-        String encodeImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-        try{
-            jsonObject = new JSONObject();
-            String imgname = idg;
-            jsonObject.put("name",imgname);
-            jsonObject.put("image",encodeImage);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String img_Url = "https://belezaonline2019.000webhostapp.com/uploadImg.php";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, img_Url, jsonObject, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.e("Erro: Resquest", jsonObject.toString());
-                requestQueue.getCache().clear();
-                Toast.makeText(getBaseContext(), "Imagem Ok", Toast.LENGTH_LONG).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyErro) {
-                Log.e("Erro: Volley", volleyErro.toString());
-            }
-        });
-
-        requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonObjectRequest);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void requestMutiPermission(){
-        Dexter.withActivity(this)
-                .withPermissions(
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport report) {
-                        if(report.areAllPermissionsGranted()){
-                            Toast.makeText(getBaseContext(), "Permissões concedidas com sucesso", Toast.LENGTH_LONG).show();
-                        }
-                        if(report.isAnyPermissionPermanentlyDenied()){
-                            Toast.makeText(getBaseContext(), "Permissões concedidas com sucesso", Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-                        token.continuePermissionRequest();
-                    }
-                }).withErrorListener(new PermissionRequestErrorListener() {
-            @Override
-            public void onError(DexterError error) {
-                Toast.makeText(getBaseContext(), "Erro na permissão", Toast.LENGTH_LONG).show();
-            }
-        }).onSameThread().check();
     }
 }
---
+++ app/src/main/java/com/evo/belezaonline_2/ImgCActivity.java
@@ -0,0 +1,189 @@
+package com.evo.belezaonline_2;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+
+public class ImgCActivity extends AppCompatActivity {
+
+    Button btUpimg;
+    ImageView imPerf;
+
+    private final int GALLERY = 1;
+    private String upload_URL = "https://belezaonline2019.000webhostapp.com/uploadImg.php";
+    String idg, nome;
+    JSONObject jsonObject;
+    RequestQueue rQueue;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img_c);
+
+        btUpimg = findViewById(R.id.btUpimg);
+        imPerf = findViewById(R.id.imPerf);
+
+        CarregarImg();
+        requestMultiplePermissions();
+
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        idg = bundle.getString("id");
+        nome = bundle.getString("nome");
+
+
+        btUpimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(galleryIntent, GALLERY);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    imPerf.setImageBitmap(bitmap);
+                    uploadImage(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(ImgCActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void uploadImage(Bitmap bitmap){
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+        try {
+            jsonObject = new JSONObject();
+            String imgname = String.valueOf(idg);
+            jsonObject.put("name", imgname);
+            //  Log.e("Image name", etxtUpload.getText().toString().trim());
+            jsonObject.put("image", encodedImage);
+            // jsonObject.put("aa", "aa");
+        } catch (JSONException e) {
+            Log.e("JSONObject Here", e.toString());
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, upload_URL, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Log.e("aaaaaaa", jsonObject.toString());
+                        rQueue.getCache().clear();
+                        Toast.makeText(getApplication(), "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("aaaaaaa", volleyError.toString());
+
+            }
+        });
+
+        rQueue = Volley.newRequestQueue(ImgCActivity.this);
+        rQueue.add(jsonObjectRequest);
+
+    }
+
+    private void  requestMultiplePermissions(){
+        Dexter.withActivity(this)
+                .withPermissions(
+
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+    private void CarregarImg(){
+        Picasso.get().load("https://belezaonline2019.000webhostapp.com/img/1.JPG").networkPolicy(NetworkPolicy.NO_CACHE).memoryPolicy(MemoryPolicy.NO_CACHE).into(imPerf);
+    }
+}
